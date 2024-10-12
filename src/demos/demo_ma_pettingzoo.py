529bd53d11da0a492d7bd7eabd58d80b81ca8f86
adriengolgdszal <adrien1.goldszal@gmail.com>
2024-10-12 22:22:28 +0200

    enable ppo loading

--- src/demos/demo_ma_pettingzoo.py
+++ src/demos/demo_ma_pettingzoo.py
@@ -9,11 +9,15 @@ import swarm_env.single_env
 from swarm_env.multi_env.multi_agent_pettingzoo import MultiSwarmEnv
 import sys
 import time
+from stable_baselines3 import PPO
+import imageio
 
 
 env = MultiSwarmEnv(render_mode="human", n_agents=2, n_targets=2, map_name="Easy")
-model = "models/ma/12-10/tn32lihj"
-total_episodes = 4
+model = "models/ma/12-10/tn32lihj/model.zip" # 2 agents and 2 targets on easy map
+model = PPO.load(model)
+total_episodes = 10
+images = []
 for i in range(total_episodes):
     
     print(f'Episode {i} of {total_episodes}')
@@ -34,5 +38,7 @@ for i in range(total_episodes):
         score += reward["agent_0"]
         if trunc["agent_0"] or ter["agent_0"]:
             print(f"Truc {trunc}, ter: {ter}, return: {score}, steps: {count}")
+            images.extend(info["ep_frames"])
             break
+    imageio.mimsave("./demo_marl.gif", images, fps=25)
     env.close()
